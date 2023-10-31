72088ea5a6743c2b2bce3d8092e4ebd2f13ee78c
abdel <abdelaziz.bensenouci2023@campus-eni.fr>
2023-10-31 13:02:00 +0100

    31/10 13h Pagination

--- views/post/index.php
+++ views/post/index.php
@@ -8,7 +8,27 @@ $title = 'Nos véhicules';
 $pdo = new PDO('mysql:dbname=projet_stage;host=127.0.0.1', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
-$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
+$page = $_GET['page'] ?? 1;
+if (!filter_var($page, FILTER_VALIDATE_INT)) {
+    throw new Exception('Numero de page invalide');
+}
+if($page ==='1') {
+    header('Location: ' .$router->url('home'));
+    http_response_code(301);
+    exit();
+}
+$currentPage = (int)$page;
+if($currentPage <= 0) {
+    throw new Exception('Numero de page invalide');
+}
+$count = (int)$pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
+$perPage = 12;
+$pages = ceil($count / $perPage);
+if($currentPage > $pages) {
+    throw new Exception('Cette page n\'existe pas');
+}
+$offset = $perPage * ($currentPage - 1);
+$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
 $posts = $query->fetchALL(PDO::FETCH_CLASS, Post::class);
 ?>
 
@@ -20,5 +40,17 @@ $posts = $query->fetchALL(PDO::FETCH_CLASS, Post::class);
         <?php require 'card.php' ?>
     </div>
     <?php endforeach ?>
+</div>
+
+<div class="d-flex justify-content-between my-4">
+    <?php if ($currentPage > 1): ?>
+        <a href="<?= $router->url('home') ?>?page=<?= $currentPage - 1 ?>" class="btn btn-primary">&laquo; Page précédente</a>
+    <?php endif ?>
+
+    <?php if ($currentPage < $pages): ?>
+        <div style="margin-left: auto;"> <!-- affichage du boutton en bas a droite lorsque l user est sur la page 1 -->
+            <a href="<?= $router->url('home') ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary">Page suivante &raquo;</a>
+        </div>
+    <?php endif ?>
 </div>
 
