b6f84c7ecb1db13a169ce3ada8035f5c86236a81
abdel <abdelaziz.bensenouci2023@campus-eni.fr>
2023-11-02 20:58:38 +0100

    02/11 mise en place du manifest (appli telechargeable)

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <link rel="manifest" href="manifest.json">
     <title><?= $title ?? 'Ma voiture' ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="icon" href="/img/auto noir.png" type="image/x-icon">
+
+
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -32,4 +35,5 @@
 <footer class="d-flex justify-content-center fixed-bottom">
     <p>© By Abdelaziz 2023</p>
 </footer>
+
 </html>
